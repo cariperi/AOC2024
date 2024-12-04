b0a94a2531160e77a41b50e73471c58c40d3ad1e
Caroline Peri <caroline.anne.peri@gmail.com>
2024-12-03 23:44:40 -0500

    working solution for challenge 2

--- day_3/day_3.rb
+++ day_3/day_3.rb
@@ -2,6 +2,10 @@
 # mul(x, y) where x and y are 1-3 digit numbers
 # ignore invalid inputs
 # sum the results of all valid mul operators
+
+# The do() instruction enables future mul instructions.
+# The don't() instruction disables future mul instructions.
+
 class Day3
   attr_reader :memory
   REGEX = /mul\((\d{1,3}),(\d{1,3})\)/
@@ -14,6 +18,12 @@ class Day3
     find_valid_muls.map { |x, y| x * y }.reduce(:+)
   end
 
+  def sum_of_valid_enabled_muls
+    operations = find_valid_muls_and_commands
+    result = handle_instructions(operations)
+    result.map { |x, y| x.to_i * y.to_i }.reduce(:+)
+  end
+
   private
 
   def get_input
@@ -23,6 +33,26 @@ class Day3
   def find_valid_muls
     memory.scan(REGEX).map { |x, y| [x.to_i, y.to_i] }
   end
+
+  def find_valid_muls_and_commands
+    memory.scan(/(mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\))/).map(&:compact)
+  end
+
+  def handle_instructions(list)
+    enabled = true
+    enabled_only = []
+    list.each do |instruction|
+      if instruction.first.include?("mul") && enabled
+        enabled_only << instruction[1..2]
+      elsif instruction.first.include?("don't")
+        enabled = false
+      elsif instruction.first.include?("do")
+        enabled = true
+      end
+    end
+    enabled_only
+  end
 end
 
 pp Day3.new.sum_of_valid_muls
+pp Day3.new.sum_of_valid_enabled_muls
